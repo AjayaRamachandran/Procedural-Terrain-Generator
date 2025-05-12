913a840b5f853681a71306878196596e136efc19
AjayaRamachandran <ajaya.ramachandran@gmail.com>
2025-05-11 22:39:20 -0400

    added scriptRoot for all file paths

--- main.py
+++ main.py
@@ -35,32 +35,32 @@ def generateMountainMap(width, length, biomes=False):
     for y, row in enumerate(image):
         for x, pixel in enumerate(row):
             gen.putpixel([y,x], (pixel, pixel, pixel))
-    gen.save("images/mountains0.png")
+    gen.save(f"{scriptRoot}\\images/mountains0.png")
     for iter in range(5):
-        img = Image.open("images/mountains" + str(iter) + ".png")
+        img = Image.open(f"{scriptRoot}\\images/mountains" + str(iter) + ".png")
         img = img.filter(ImageFilter.GaussianBlur((iter + 1)**1.4))
         enhancer = ImageEnhance.Contrast(img)
         img = enhancer.enhance(1.9)
         #img = img.transpose(Image.ROTATE_90)
-        img.save("images/mountains" + str(iter + 1) + ".png")
+        img.save(f"{scriptRoot}\\images/mountains" + str(iter + 1) + ".png")
     
     opacities = [0.9, 0.8, 0.7, 0.5, 0.6]
     for iter in range(5):
-        background = Image.blend(Image.open(f"images/mountains{iter}.png"), Image.open(f"images/mountains{iter + 1}.png"), opacities[iter])
-        background.save(f"images/mountains{iter + 1}.png")
+        background = Image.blend(Image.open(f"{scriptRoot}\\images/mountains{iter}.png"), Image.open(f"{scriptRoot}\\images/mountains{iter + 1}.png"), opacities[iter])
+        background.save(f"{scriptRoot}\\images/mountains{iter + 1}.png")
 
     if biomes:
         for iter in range(5,10):
-            img = Image.open("images/mountains" + str(iter) + ".png")
+            img = Image.open(f"{scriptRoot}\\images/mountains" + str(iter) + ".png")
             img = img.filter(ImageFilter.GaussianBlur((iter + 1)**1.5))
             enhancer = ImageEnhance.Contrast(img)
             img = enhancer.enhance(1.5)
             #img = img.transpose(Image.ROTATE_90)
-            img.save("images/mountains" + str(iter + 1) + ".png")
+            img.save(f"{scriptRoot}\\images/mountains" + str(iter + 1) + ".png")
         opacities = [0.4, 0.4, 0.3, 0.3, 0.2]
         for iter in range(5,10):
-            background = Image.blend(Image.open(f"images/mountains{iter}.png"), Image.open(f"images/mountains{iter + 1}.png"), opacities[iter - 5])
-            background.save(f"images/mountains{iter + 1}.png")
+            background = Image.blend(Image.open(f"{scriptRoot}\\images/mountains{iter}.png"), Image.open(f"{scriptRoot}\\images/mountains{iter + 1}.png"), opacities[iter - 5])
+            background.save(f"{scriptRoot}\\images/mountains{iter + 1}.png")
     
     min = 99999
     max = 0
@@ -172,7 +172,7 @@ def generateCrackMap(width, length):
     for y, row in enumerate(image):
         for x, pixel in enumerate(row):
             gen.putpixel([y,x], (pixel, pixel, pixel))
-    gen.save("images/map.png")
+    gen.save(f"{scriptRoot}\\images/map.png")
     return image
 
 def generateBiomeMap(width, length):
@@ -201,12 +201,12 @@ def generateBiomeMap(width, length):
                 gen.putpixel([x, y], tuple(colors[0][1]))
                 rowBiomes.append(colors[0][0])
         biomeMap.append(rowBiomes)
-    gen.save("images/biomes.png")
+    gen.save(f"{scriptRoot}\\images/biomes.png")
     return biomeMap
 
 def generateIslandShape(length, width):
     global biomeMap
-    spawnfile = json.load(open(f"{scriptRoot}spawnfile.json"))
+    spawnfile = json.load(open(f"{scriptRoot}\\spawnfile.json"))
     ores = spawnfile['ORES']
     stoneVariants = spawnfile['STONE VARIANTS']
     shrubs = spawnfile['SHRUBS']
@@ -726,7 +726,7 @@ def convertToJson(version):
 
                     schemData.append(block)
 
-    json.dump(schemData, open("output.json", "w"))
+    json.dump(schemData, open(f"{scriptRoot}\\output.json", "w"))
 
 
 ###### MAIN ######
